543a6a24ad8372262b287ba9d041e6b2d87a017d
Sam Bailey <samabailey6194@gmail.com>
2025-01-21 09:21:22 +0000

    Create empty functions with explanation for what they do

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,67 @@
+/**
+ * Reveal username creation box to user
+ */
 function username() {
 
 }
 
+/**
+ * Show guide to user after creating user
+ * or anytime they click show guide button
+ */
 function showGuide() {
 
 }
 
-function showQuiz() {
-
-}
-
+/**
+ * Questions, options and answered stored here
+ */
 const quizData = {
 
 }
 
-function showQuestion() {
+/**
+ * Reveal quiz and randomize order of questions
+ */
+function showQuiz() {
+    function showQuestion() {
+
+    }
 
 }
 
+/**
+ * Checks answer user gives to the correct answer
+ */
 function selectAnswer() {
 
 }
 
+/**
+ * Increases correct score by 1 if user gets answer right
+ */
 function incrementScore() {
 
 }
 
+/**
+ * Increases incorrect score by 1 if user gets answer right
+ */
 function incrementWrong() {
 
 }
 
+/**
+ * Reveals an alert box to show the user if they got it right
+ * if they got it wrong it tells them what the answer was
+ */
 function showResult() {
+
+}
+
+/**
+ * Reveals leaderboard once quiz is complete
+ */
+function showLeaderboard() {
 
 }
