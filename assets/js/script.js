f94c65993b769a3ed3bac455c0376e906cf5014f
Sam Bailey <samabailey6194@gmail.com>
2025-01-21 17:16:17 +0000

    Add function to showquestion and change go to leaderboard button to hidden class

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,11 +86,11 @@ function reveal() {
 
 
 // Fisher-Yates Algorithim to help randomize order of questions
-function shuffle(array){
+function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const random = Math.floor(Math.random() * (i + 1));
 
-        [array[i], array[random] = array[random], array [i]];
+        [array[i], array[random] = array[random], array[i]];
     }
     return array;
 };
@@ -99,43 +99,42 @@ function shuffle(array){
  * Questions, options and answered stored here
  */
 const quizData = [{
-    question: "It is Christmas, no need to be afraid. We are to spread a smile of joy and pray for the other ones. While you pray we are to feed the world too and let them know it is Christmas.",
-    options: ["Blue Christmas", "Jingle Bell Rock", "Do they know it's Christmas", "Santa Baby"],
-    answer: "Do they know it's Christmas"
-},
-{
-    question: "There is not a lot I want for Christmas, only one thing I need. Santa will not make me happy. Please make my wish come true. I definitely will not ask for much this Christmas. Please just come and hold me tight. I just want to see my baby.",
-    options: ["All I want for Christmas", "It's Beginning to look a lot like Christmas", "Feliz Navidad", "White Christmas"],
-    answer: "All I want for Christmas"
-},
-{
-    question: "On Christmas Eve Sinatra was swinging and we kissed on a corner and danced through the night. The boys of the NYPD were singing Galway Bay and the bells rang out for Christmas day.",
-    options: ["Santa Claus is Coming to Town", "Fairytale of New York", "Merry Xmas Everybody", "I wish it could be Christmas everyday"],
-    answer: "Fairytale of New York"
-},
-{
-    question: "There was no crib. But little Lord Jesus lay down his head. The stars looked down on Him while he was asleep. The cattle were lowing as He awoke with no crying.",
-    options: ["O Come All Ye Faithful", "We Three Kings", "Silent Night", "Away in a Manger"],
-    answer: "Away in a Manger"
-},
-{
-    question: "The angels sing and give glory to a new born King! There is a lot of joy to be proclaimed for Christ is born in Bethlehem. Christ is adored and is the everlasting Lord. Born so no man may die.",
-    options: ["Hark the Herald", "Little Donkey", "While Shepherds Watched", "Joy to the World"],
-    answer: "Hark the Herald"
-},
-{
-    question: "Israel is captive and mourns in lonely exile. But rejoice for Emmanuel shall come. He ordered all things and gave us a path of knowledge. Israel received the law and were in awe. They were expecting a branch from Jesse's family.",
-    options: ["O Little Town of Bethlehem", "O Come, O Come Emmanuel", "In the Bleak Midwinter", "The First Noel"],
-    answer: "O Come, O Come Emmanuel"
-},
+        question: "It is Christmas, no need to be afraid. We are to spread a smile of joy and pray for the other ones. While you pray we are to feed the world too and let them know it is Christmas.",
+        options: ["Blue Christmas", "Jingle Bell Rock", "Do they know it's Christmas", "Santa Baby"],
+        answer: "Do they know it's Christmas"
+    },
+    {
+        question: "There is not a lot I want for Christmas, only one thing I need. Santa will not make me happy. Please make my wish come true. I definitely will not ask for much this Christmas. Please just come and hold me tight. I just want to see my baby.",
+        options: ["All I want for Christmas", "It's Beginning to look a lot like Christmas", "Feliz Navidad", "White Christmas"],
+        answer: "All I want for Christmas"
+    },
+    {
+        question: "On Christmas Eve Sinatra was swinging and we kissed on a corner and danced through the night. The boys of the NYPD were singing Galway Bay and the bells rang out for Christmas day.",
+        options: ["Santa Claus is Coming to Town", "Fairytale of New York", "Merry Xmas Everybody", "I wish it could be Christmas everyday"],
+        answer: "Fairytale of New York"
+    },
+    {
+        question: "There was no crib. But little Lord Jesus lay down his head. The stars looked down on Him while he was asleep. The cattle were lowing as He awoke with no crying.",
+        options: ["O Come All Ye Faithful", "We Three Kings", "Silent Night", "Away in a Manger"],
+        answer: "Away in a Manger"
+    },
+    {
+        question: "The angels sing and give glory to a new born King! There is a lot of joy to be proclaimed for Christ is born in Bethlehem. Christ is adored and is the everlasting Lord. Born so no man may die.",
+        options: ["Hark the Herald", "Little Donkey", "While Shepherds Watched", "Joy to the World"],
+        answer: "Hark the Herald"
+    },
+    {
+        question: "Israel is captive and mourns in lonely exile. But rejoice for Emmanuel shall come. He ordered all things and gave us a path of knowledge. Israel received the law and were in awe. They were expecting a branch from Jesse's family.",
+        options: ["O Little Town of Bethlehem", "O Come, O Come Emmanuel", "In the Bleak Midwinter", "The First Noel"],
+        answer: "O Come, O Come Emmanuel"
+    },
 ]
-
-const randomQuestions = shuffle(quizData);
-const randomOptions = shuffle(quizData.options);
 
 const questionElement = document.getElementById("question");
 const optionElement = document.getElementById("options");
 const answerButton = document.getElementById("answer");
+
+const randomQuestion = shuffle(quizData);
 
 /**
  * Reveal questions in random order
@@ -143,11 +142,21 @@ const answerButton = document.getElementById("answer");
 function showQuestion() {
 
     // Increase question number we are on
-    let oldQuestionNo = parseInt (document.getElementById('questionno').innerText);
+    let oldQuestionNo = parseInt(document.getElementById('questionno').innerText);
     document.getElementById('questionno').innerText = ++oldQuestionNo;
 
     // Reveal random question
+    const currentQuestion = quizData[randomQuestion];
+    questionElement.innerText = currentQuestion.question;
 
+    optionsElement.innerHTML = "";
+    currentQuestion.options.forEach(option => {
+        const button = document.createElement("button");
+        button.innerText = option;
+        button.classList.add("submit-btn")
+        optionsElement.appendChild(button);
+        button.addEventListener("click", selectAnswer);
+    });
 }
 
 /**
