8e033235322e0c2778b2903220d227fa8a0e12d0
Sam Bailey <samabailey6194@gmail.com>
2025-01-21 11:08:53 +0000

    Create questions for the quiz with options and answers written in

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,42 @@ let lboardReveal = document.getElementById("lboard");
 /**
  * Reveal the div the user asks for when clicking relevant button
  */
-function reveal() {
-    let 
-}
+function reveal() {}
 
 /**
  * Questions, options and answered stored here
  */
-const quizData = {
-
-}
+const quizData = [{
+        question: "It is Christmas, no need to be afraid. We are to spread a smile of joy and pray for the other ones. While you pray we are to feed the world too and let them know it is Christmas.",
+        options: ["Blue Christmas", "Jingle Bell Rock", "Do they know it's Christmas", "Santa Baby"],
+        answer: "Do they know it's Christmas"
+    },
+    {
+        question: "There is not a lot I want for Christmas, only one thing I need. Santa will not make me happy. Please make my wish come true. I definitely will not ask for much this Christmas. Please just come and hold me tight. I just want to see my baby.",
+        options: ["All I want for Christmas", "It's Beginning to look a lot like Christmas", "Feliz Navidad", "White Christmas"],
+        answer: "All I want for Christmas"
+    },
+    {
+        question: "On Christmas Eve Sinatra was swinging and we kissed on a corner and danced through the night. The boys of the NYPD were singing Galway Bay and the bells rang out for Christmas day.",
+        options: ["Santa Claus is Coming to Town", "Fairytale of New York", "Merry Xmas Everybody", "I wish it could be Christmas everyday"],
+        answer: "Fairytale of New York"
+    },
+    {
+        question: "There was no crib. But little Lord Jesus lay down his head. The stars looked down on Him while he was asleep. The cattle were lowing as He awoke with no crying.",
+        options: ["O Come All Ye Faithful", "We Three Kings", "Silent Night", "Away in a Manger"],
+        answer: "Away in a Manger"
+    },
+    {
+        question: "The angels sing and give glory to a new born King! There is a lot of joy to be proclaimed for Christ is born in Bethlehem. Christ is adored and is the everlasting Lord. Born so no man may die.",
+        options: ["Hark the Herald", "Little Donkey", "While Shepherds Watched", "Joy to the World"],
+        answer: "Hark the Herald"
+    },
+    {
+        question: "Israel is captive and mourns in lonely exile. But rejoice for Emmanuel shall come. He ordered all things and gave us a path of knowledge. Israel received the law and were in awe. They were expecting a branch from Jesse's family.",
+        options: ["O Little Town of Bethlehem", "O Come, O Come Emmanuel", "In the Bleak Midwinter", "The First Noel"],
+        answer: "O Come, O Come Emmanuel"
+    },
+]
 
 /**
  * Reveal questions in random order
