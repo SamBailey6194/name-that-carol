32e0b0c900ebb2e7b0ac83c581aa5e96a050bbfc
SamBailey6194 <samabailey6194@gmail.com>
2025-01-24 16:07:41 +0000

    Add correct JS code to select answer to allow user to hit submit after selecting their answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,88 +7,90 @@ const lboardDiv = document.getElementById("lboard");
 /**
  * Validating username creation
  */
-document.addEventListener('DOMContentLoaded', () => {
-    // Check if a username is already saved and display it
-    const savedUsername = localStorage.getItem('username');
-    const message = document.getElementById('message');
+document.addEventListener("DOMContentLoaded", () => {
+  // Check if a username is already saved and display it
+  const savedUsername = localStorage.getItem("username");
+  const message = document.getElementById("message");
 
-    if (savedUsername) {
-        message.textContent = `Welcome back, ${savedUsername}! Give the quiz another try and beat your best score!`;
-        document.getElementById("guide-btn").classList.remove('hidden');
-        document.getElementById("quiz-btn").classList.remove('hidden');
-    }
+  if (savedUsername) {
+    message.textContent = `Welcome back, ${savedUsername}! Give the quiz another try and beat your best score!`;
+    document.getElementById("guide-btn").classList.remove("hidden");
+    document.getElementById("quiz-btn").classList.remove("hidden");
+  }
 
-    // Form submission handler
-    const form = document.getElementById('formuser');
-    form.addEventListener('submit', function (event) {
-        // Prevents form from reloading the page
-        event.preventDefault();
+  // Form submission handler
+  const form = document.getElementById("formuser");
+  form.addEventListener("submit", function (event) {
+    // Prevents form from reloading the page
+    event.preventDefault();
 
-        // Get entered username
-        const usernameInput = document.getElementById('username').value;
+    // Get entered username
+    const usernameInput = document.getElementById("username").value;
 
-        //Save username in localStorage
-        localStorage.setItem('username', usernameInput);
+    //Save username in localStorage
+    localStorage.setItem("username", usernameInput);
 
-        // display a message and buttons
-        message.textContent = `Username saved: ${usernameInput}! Read the guide first and then enjoy the quiz!`
-        document.getElementById("guide-btn").classList.remove('hidden');
-        document.getElementById("quiz-btn").classList.remove('hidden');
+    // display a message and buttons
+    message.textContent = `Username saved: ${usernameInput}! Read the guide first and then enjoy the quiz!`;
+    document.getElementById("guide-btn").classList.remove("hidden");
+    document.getElementById("quiz-btn").classList.remove("hidden");
+  });
+
+  // Allow user to hit enter key when typing in username
+  document
+    .getElementById("username")
+    .addEventListener("keydown", function (event) {
+      if (event === "Enter") {
+        usernameInput;
+      }
     });
-
-    // Allow user to hit enter key when typing in username
-    document.getElementById('username').addEventListener("keydown", function (event) {
-        if (event === "Enter") {
-            usernameInput
-        }
-    })
 });
 
 /**
  * Reveal the div the user asks for when clicking relevant button
  */
 function reveal() {
-    const showGuide = document.getElementById("guide-btn");
-    const showQuiz = document.getElementById("quiz-btn");
-    const showLeaderboard = document.getElementById("lboard-btn");
-    const showGuide1 = document.getElementById("guide-btn1");
-    const showQuiz1 = document.getElementById("quiz-btn1");
-    const showQuiz2 = document.getElementById("quiz-btn2");
+  const showGuide = document.getElementById("guide-btn");
+  const showQuiz = document.getElementById("quiz-btn");
+  const showLeaderboard = document.getElementById("lboard-btn");
+  const showGuide1 = document.getElementById("guide-btn1");
+  const showQuiz1 = document.getElementById("quiz-btn1");
+  const showQuiz2 = document.getElementById("quiz-btn2");
 
-    // Reveal guide and hide username creation
-    showGuide.addEventListener("click", function () {
-        usernameDiv.classList.add("hidden");
-        guideDiv.classList.remove("hidden");
-    })
+  // Reveal guide and hide username creation
+  showGuide.addEventListener("click", function () {
+    usernameDiv.classList.add("hidden");
+    guideDiv.classList.remove("hidden");
+  });
 
-    // Reveal quiz and hide username creation
-    showQuiz.addEventListener("click", function () {
-        usernameDiv.classList.add("hidden");
-        quizDiv.classList.remove("hidden");
-    })
+  // Reveal quiz and hide username creation
+  showQuiz.addEventListener("click", function () {
+    usernameDiv.classList.add("hidden");
+    quizDiv.classList.remove("hidden");
+  });
 
-    // Reveal leaderboard and hide quiz
-    showLeaderboard.addEventListener("click", function () {
-        quizDiv.classList.add("hidden");
-        lboardDiv.classList.remove("hidden");
-    })
+  // Reveal leaderboard and hide quiz
+  showLeaderboard.addEventListener("click", function () {
+    quizDiv.classList.add("hidden");
+    lboardDiv.classList.remove("hidden");
+  });
 
-    // Reveal guide above quiz
-    showGuide1.addEventListener("click", function () {
-        guideDiv.classList.remove("hidden");
-    })
+  // Reveal guide above quiz
+  showGuide1.addEventListener("click", function () {
+    guideDiv.classList.remove("hidden");
+  });
 
-    // Reveal quiz and hide guide
-    showQuiz1.addEventListener("click", function () {
-        guideDiv.classList.add("hidden");
-        quizDiv.classList.remove("hidden");
-    })
+  // Reveal quiz and hide guide
+  showQuiz1.addEventListener("click", function () {
+    guideDiv.classList.add("hidden");
+    quizDiv.classList.remove("hidden");
+  });
 
-    // Reveal quiz and hide leaderboard
-    showQuiz2.addEventListener("click", function () {
-        lboardDiv.classList.add("hidden");
-        quizDiv.classList.remove("hidden");
-    })
+  // Reveal quiz and hide leaderboard
+  showQuiz2.addEventListener("click", function () {
+    lboardDiv.classList.add("hidden");
+    quizDiv.classList.remove("hidden");
+  });
 }
 
 /**
@@ -98,40 +100,76 @@ function reveal() {
 /**
  * Questions, options and answered stored here
  */
-const quizData = [{
-        question: "It is Christmas, no need to be afraid. We are to spread a smile of joy and pray for the other ones. While you pray we are to feed the world too and let them know it is Christmas.",
-        options: ["Blue Christmas", "Jingle Bell Rock", "Do they know it's Christmas", "Santa Baby"],
-        answer: "Do they know it's Christmas",
-    },
-    {
-        question: "There is not a lot I want for Christmas, only one thing I need. Santa will not make me happy. Please make my wish come true. I definitely will not ask for much this Christmas. Please just come and hold me tight. I just want to see my baby.",
-        options: ["All I want for Christmas", "It's Beginning to look a lot like Christmas", "Feliz Navidad", "White Christmas"],
-        answer: "All I want for Christmas",
-    },
-    {
-        question: "On Christmas Eve Sinatra was swinging and we kissed on a corner and danced through the night. The boys of the NYPD were singing Galway Bay and the bells rang out for Christmas day.",
-        options: ["Santa Claus is Coming to Town", "Fairytale of New York", "Merry Xmas Everybody", "I wish it could be Christmas everyday"],
-        answer: "Fairytale of New York",
-    },
-    {
-        question: "There was no crib. But little Lord Jesus lay down his head. The stars looked down on Him while he was asleep. The cattle were lowing as He awoke with no crying.",
-        options: ["O Come All Ye Faithful", "We Three Kings", "Silent Night", "Away in a Manger"],
-        answer: "Away in a Manger",
-    },
-    {
-        question: "The angels sing and give glory to a new born King! There is a lot of joy to be proclaimed for Christ is born in Bethlehem. Christ is adored and is the everlasting Lord. Born so no man may die.",
-        options: ["Hark the Herald", "Little Donkey", "While Shepherds Watched", "Joy to the World"],
-        answer: "Hark the Herald",
-    },
-    {
-        question: "Israel is captive and mourns in lonely exile. But rejoice for Emmanuel shall come. He ordered all things and gave us a path of knowledge. Israel received the law and were in awe. They were expecting a branch from Jesse's family.",
-        options: ["O Little Town of Bethlehem", "O Come, O Come Emmanuel", "In the Bleak Midwinter", "The First Noel"],
-        answer: "O Come, O Come Emmanuel",
-    },
-]
+const quizData = [
+  {
+    question:
+      "It is Christmas, no need to be afraid. We are to spread a smile of joy and pray for the other ones. While you pray we are to feed the world too and let them know it is Christmas.",
+    options: [
+      "Blue Christmas",
+      "Jingle Bell Rock",
+      "Do they know it's Christmas",
+      "Santa Baby",
+    ],
+    answer: "Do they know it's Christmas",
+  },
+  {
+    question:
+      "There is not a lot I want for Christmas, only one thing I need. Santa will not make me happy. Please make my wish come true. I definitely will not ask for much this Christmas. Please just come and hold me tight. I just want to see my baby.",
+    options: [
+      "All I want for Christmas",
+      "It's Beginning to look a lot like Christmas",
+      "Feliz Navidad",
+      "White Christmas",
+    ],
+    answer: "All I want for Christmas",
+  },
+  {
+    question:
+      "On Christmas Eve Sinatra was swinging and we kissed on a corner and danced through the night. The boys of the NYPD were singing Galway Bay and the bells rang out for Christmas day.",
+    options: [
+      "Santa Claus is Coming to Town",
+      "Fairytale of New York",
+      "Merry Xmas Everybody",
+      "I wish it could be Christmas everyday",
+    ],
+    answer: "Fairytale of New York",
+  },
+  {
+    question:
+      "There was no crib. But little Lord Jesus lay down his head. The stars looked down on Him while he was asleep. The cattle were lowing as He awoke with no crying.",
+    options: [
+      "O Come All Ye Faithful",
+      "We Three Kings",
+      "Silent Night",
+      "Away in a Manger",
+    ],
+    answer: "Away in a Manger",
+  },
+  {
+    question:
+      "The angels sing and give glory to a new born King! There is a lot of joy to be proclaimed for Christ is born in Bethlehem. Christ is adored and is the everlasting Lord. Born so no man may die.",
+    options: [
+      "Hark the Herald",
+      "Little Donkey",
+      "While Shepherds Watched",
+      "Joy to the World",
+    ],
+    answer: "Hark the Herald",
+  },
+  {
+    question:
+      "Israel is captive and mourns in lonely exile. But rejoice for Emmanuel shall come. He ordered all things and gave us a path of knowledge. Israel received the law and were in awe. They were expecting a branch from Jesse's family.",
+    options: [
+      "O Little Town of Bethlehem",
+      "O Come, O Come Emmanuel",
+      "In the Bleak Midwinter",
+      "The First Noel",
+    ],
+    answer: "O Come, O Come Emmanuel",
+  },
+];
 
 // Fisher-Yates Algorithim to help randomize order of questions
-
 
 const questionElement = document.getElementById("question");
 const optionElement = document.getElementById("options");
@@ -141,74 +179,74 @@ const answerButton = document.getElementById("answer-btn");
  * Reveal questions in random order
  */
 function showQuestion() {
+  // Increase question number we are on
+  let oldQuestionNo = parseInt(document.getElementById("questionno").innerText);
+  document.getElementById("questionno").innerText = ++oldQuestionNo;
 
-    // Increase question number we are on
-    let oldQuestionNo = parseInt(document.getElementById('questionno').innerText);
-    document.getElementById('questionno').innerText = ++oldQuestionNo;
+  // Reveal random question
+  let currentQuestion = 0;
+  let question = quizData[currentQuestion];
+  questionElement.innerText = question.question;
 
-    // Reveal random question
-    let currentQuestion = 0;
-    let question = quizData[currentQuestion];
-    questionElement.innerText = question.question;
-
-    // Reveal random options
-    optionElement.innerHTML = "";
-    question.options.forEach(option => {
-        const button = document.createElement("button");
-        button.innerText = option;
-        optionElement.appendChild(button);
-        button.addEventListener("click", selectAnswer);
-    });
+  // Reveal random options
+  optionElement.innerHTML = "";
+  question.options.forEach((option) => {
+    const button = document.createElement("button");
+    button.innerText = option;
+    optionElement.appendChild(button);
+    button.addEventListener("click", selectAnswer);
+  });
 }
 
 /**
  * Checks answer user gives to the correct answer
  */
-function selectAnswer(e) {
+function selectAnswer() {
+  answerButton.addEventListener("submit", function (e) {
     const selectedButton = e.target;
-    const answer = question.answer;
-
+    const answer = quizData[question].answer;
 
     if (selectedButton.innerText === answer) {
-        alert("Well done! You got it right!")
-        incrementScore();
+      alert("Well done! You got it right!");
+      incrementScore();
     } else {
-        alert(`Unlucky you got it wrong. The answer was ${question.answer}`)
-        incrementWrong();
+      alert(`Unlucky you got it wrong. The answer was ${quizData[question].answer}`);
+      incrementWrong();
     }
 
     question++;
 
     if (question < quizData.length) {
-        showQuestion();
+      showQuestion();
     } else {
-        showResult();
+      showResult();
     }
+  });
 }
 
 /**
  * Increases correct score by 1 if user gets answer right
  */
 function incrementScore() {
-    const correctScore = parseInt(document.getElementById('correct').innerText);
-    document.getElementById('correct').innerText = ++correctScore;
+  const correctScore = parseInt(document.getElementById("correct").innerText);
+  document.getElementById("correct").innerText = ++correctScore;
 }
 
 /**
  * Increases incorrect score by 1 if user gets answer right
  */
 function incrementWrong() {
-    const wrongScore = parseInt(document.getElementById('wrong').innerText);
-    document.getElementById('wrong').innerText = ++wrongScore;
+  const wrongScore = parseInt(document.getElementById("wrong").innerText);
+  document.getElementById("wrong").innerText = ++wrongScore;
 }
 
 /**
  * Shows result of overall quiz when they finish
  */
 function showResult() {
-    quizData.innerHTML = `
+  quizData.innerHTML = `
     <h1>Quiz Completed!</h1>
-    <p>Your score: ${correctScore}/${quizData.length}</p>`
+    <p>Your score: ${correctScore}/${quizData.length}</p>`;
 }
 
 showQuestion();
